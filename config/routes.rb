82aa32554e3621fb026871111a5b4aa00048ed07
takanori-hagiwara <allhappyhagi@outlook.com>
2021-02-10 12:18:44 +0900

    コメントが投稿修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
   resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :comments, only: :show
+    resources :comments, only: :create
   end 
   resources :users, only: :show
 end
